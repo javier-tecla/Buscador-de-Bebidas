147829b51e00fd14f0504d6ea5307474367f571e
javierborjas <cristman11@gmail.com>
2024-02-13 16:10:33 -0300

    Agregando el Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap'
+import Formulario from './components/Formulario'
 
 function App() {
   
@@ -10,7 +11,7 @@ function App() {
 
 
       <Container className='mt-5'>
-          1
+          <Formulario />
       </Container>
     </>
   )
